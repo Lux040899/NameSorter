91b6fe28713f6a51cd0809bd892014e0154b4319
Lakshya Mittal <lakshya.mittal@globalx.com.au>
2020-02-05 14:06:15 +1100

    Restoring all the data

---
+++ NameSorterAlpha/NameSorterAlpha/Name.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace NameSorterAlpha
+{
+    class Name : IComparable<Name>
+    {
+        private string name;
+        private string Lastname;
+        private int index_of_last_name;
+        public Name(string name)
+        {
+            this.name = name;
+            // Finding the first index of Last Name
+            index_of_last_name = name.LastIndexOf(' ') + 1;
+            this.Lastname = name.Substring(index_of_last_name);
+        }
+
+        public int CompareTo(Name other)
+        {
+            if (this.Lastname.CompareTo(other.Lastname) == 0)
+            {
+                return this.ComparePreviousNames(other);
+            }
+
+            else return this.Lastname.CompareTo(other.Lastname);
+        }
+
+        public int ComparePreviousNames(Name other)
+        {
+            int result = 0;
+
+            while (this.index_of_last_name > 0 && other.index_of_last_name > 0)
+            {
+                result = this.getPreviousName().CompareTo(other.getPreviousName());
+                if (result != 0)
+                {
+                    this.ResetLastNameIndex(other);
+                    return result;
+                }
+            }
+
+
+            if (this.index_of_last_name == 0 && other.index_of_last_name > 0)
+            {
+                this.ResetLastNameIndex(other);
+                return -1;
+            }
+            else if (this.index_of_last_name > 0 && other.index_of_last_name == 0)
+            {
+                this.ResetLastNameIndex(other);
+                return 1;
+            }
+
+            this.ResetLastNameIndex(other);
+
+            return result;
+        }
+
+        public string getPreviousName()
+        {
+            string PreviousName;
+            int index_tracker = index_of_last_name - 2;
+
+            while (index_tracker > 0 && this.name[index_tracker] != ' ') index_tracker--;
+
+            if (index_tracker != 0)
+            {
+                PreviousName = this.name.Substring(index_tracker + 1, index_of_last_name - index_tracker - 2);
+                this.index_of_last_name = index_tracker + 1;
+            }
+
+            else
+            {
+                PreviousName = this.name.Substring(index_tracker, index_of_last_name - 1);
+                this.index_of_last_name = index_tracker;
+            }
+
+            return PreviousName;
+        }
+
+        public void ResetLastNameIndex(Name other)
+        {
+            this.index_of_last_name = this.name.LastIndexOf(' ') + 1;
+            other.index_of_last_name = other.name.LastIndexOf(' ') + 1;
+
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+    }
+
+}
---
+++ NameSorterAlpha/NameSorterAlpha/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace NameSorterAlpha
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Name> names = new List<Name>();
+            List<string> sortNameList = new List<string>();
+
+            // Initialising the list of names
+            foreach (string name in File.ReadLines(args[0]))
+            {
+                names.Add(new Name(name));
+            }
+
+            names.Sort();
+
+            // Initialsiing the list of sorted names
+            foreach (Name name in names)
+            {
+                Console.WriteLine(name.getName());
+                sortNameList.Add(name.getName());
+            }
+
+            File.WriteAllLines("sorted-names-list.txt", sortNameList);
+            Console.ReadKey();
+        }
+
+    }
+
+}
