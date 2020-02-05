e61fc951d9c21753b23ca0b305358fb9f473a0e8
Lakshya Mittal <lakshya.mittal@globalx.com.au>
2020-02-05 14:28:39 +1100

    Adding an alternate approach

---
+++ NameSorterAlpha/NameSorterAlpha/AlternateName.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter 
+{
+    class AlternateName : IComparable<AlternateName>
+    {
+        private string name;
+        private string reversed_name;
+
+        public AlternateName(string name)
+        {
+            this.name = name;
+            string[] nameArray = name.Split(' ');
+            Array.Reverse(nameArray);
+            foreach (string name_component in nameArray)
+            {
+                reversed_name += name_component;
+                reversed_name += ' ';
+            }
+        }
+
+        public int CompareTo(AlternateName other)
+        {
+            return this.reversed_name.CompareTo(other.reversed_name);
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+}
--- NameSorterAlpha/NameSorterAlpha/Name.cs
+++ NameSorterAlpha/NameSorterAlpha/Name.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace NameSorterAlpha
+namespace NameSorter
 {
     class Name : IComparable<Name>
     {
