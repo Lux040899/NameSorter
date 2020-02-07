4fae3bcbacbf66f158dbc9d8ba9bd3a3b78d1165
lakshya.mittal <lakshya.mittal@GX02174>
2020-02-07 15:14:58 +1100

    Normal editing

--- NameSorterAlpha/NameSorterAlpha/Program.cs
+++ NameSorterAlpha/NameSorterAlpha/Program.cs
@@ -8,18 +8,28 @@ namespace NameSorter
     {
         public static void Main(string[] args)
         {
-            //List<Name> names = new List<Name>();
+            // List<Name> names = new List<Name>();
             List<AlternateName> names = new List<AlternateName>();
             List<string> sortNameList = new List<string>();
 
             // Initialising the list of names
             foreach (string name in File.ReadLines(args[0]))
             {
+                // names.Add(new Name(name));
                 names.Add(new AlternateName(name));
             }
 
             names.Sort();
 
+
+            /*
+            foreach (Name name in names)
+            {
+                Console.WriteLine(name.getName());
+                sortNameList.Add(name.getName());
+            }            
+            */
+            
             // Initialsiing the list of sorted names
             foreach (AlternateName name in names)
             {
